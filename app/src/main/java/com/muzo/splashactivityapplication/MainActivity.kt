cedbd3b86bdd86811acc778763278e745d1110a8
erkocali1 <muhammedalierkoc92@gmail.com>
2023-07-06 18:19:47 +0300

    finally project .

--- app/src/main/java/com/muzo/splashactivityapplication/MainActivity.kt
+++ app/src/main/java/com/muzo/splashactivityapplication/MainActivity.kt
@@ -3,10 +3,16 @@ package com.muzo.splashactivityapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        installSplashScreen().apply {
+            runBlocking {
+                delay(1500)
+            }
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
